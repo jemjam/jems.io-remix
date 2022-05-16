aac500ded0c483b5e410dd9d1e1358515617098d
Jem (Jam) Bezooyen <github@jem.dev>
2022-05-16 07:38:10 -0700

    feat(sanity-db): Change to fresh sanity projectId
    The sanity project updated to a fresh db (to start over from scratch).
    We need our frontend to point at it.
    
    - Update projectId

--- lib/sanity/config.ts
+++ lib/sanity/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   apiVersion: "2021-10-21",
-  projectId: "noabyjmx",
+  projectId: "vet84lmg",
   dataset: "production",
   useCdn: false,
   // token: process.env.SANITY_API_TOKEN ?? ``,
